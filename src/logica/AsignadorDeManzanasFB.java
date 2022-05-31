23ef02221faf3dd9c70cefd6b77df6825604e6de
Mariano Chun <marianochun01@gmail.com>
2022-05-31 13:27:08 -0300

    Principio de solucion fuerza bruta

--- src/logica/AsignadorDeManzanasFB.java
+++ src/logica/AsignadorDeManzanasFB.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.stream.IntStream;
 
 import org.apache.poi.ss.formula.eval.NotImplementedException;
 
@@ -12,6 +13,9 @@ public class AsignadorDeManzanasFB {
 	private ArrayList<Censista> censistas;
 	private ArrayList<Manzana> manzanas;
 	
+	private ArrayList<ArrayList<Manzana>> grupos;
+	private ArrayList<Manzana> grupoActual;
+	
 	public AsignadorDeManzanasFB(ArrayList<Censista> censistas, RadioCensal radioCensal) {
 		this.censistas = censistas;
 		this.radioCensal = radioCensal;
@@ -19,6 +23,11 @@ public class AsignadorDeManzanasFB {
 		// Ordenado de menor a mayor de acuerdo al grado de la manzana (cant vecinos)
 		Collections.sort(manzanas,
 				(p, q) -> radioCensal.gradoManzana(q.getNroManzana()) - radioCensal.gradoManzana(p.getNroManzana()));
+		
+		
+		this.grupos = new ArrayList<ArrayList<Manzana>>();
+		this.grupoActual = new ArrayList<Manzana>();
+//		construirGrupoDeManzanasAsignables(0);
 	}
 	
 	public void asignarManzanasACensistas() {
@@ -27,8 +36,39 @@ public class AsignadorDeManzanasFB {
 		 */
 	}
 	
-	private ArrayList<ArrayList<Manzana>> construirGrupoDeManzanasAsignables(int nroManzana){
-		throw new NotImplementedException("No implementado");
+	
+//	private ArrayList<Integer> obtenerNrosManzanas(){
+//		ArrayList<Integer> nrosManzanas = new ArrayList<Integer>();
+//		for(Manzana manzana : manzanas) {
+//			nrosManzanas.add(manzana.getNroManzana());
+//		}
+//		
+//		return nrosManzanas;
+//	}
+	
+	public ArrayList<ArrayList<Manzana>> getGrupos() {
+		return grupos;
+	}
+	// El criterio para definir que una solucion es mejor que otra es si tiene menos grupos asignables.
+	// La mejor solucion es la que tenga la menor cantidad de grupos asignables posibles.
+	public void construirGrupoDeManzanasAsignables(int nroManzana){
+//		grupoActual.stream().forEach(m -> System.out.print(m.getNroManzana()));
+//		System.out.println();
+		
+		if(nroManzana == radioCensal.cantManzanas()) {
+			return;
+		}
+		if(grupoActual.size() <= 3 && esCamino(grupoActual)) {
+//			System.out.println("Pasa " + grupoActual);
+			grupos.add(grupoActual);
+		}
+		
+		grupoActual.add(radioCensal.getManzana(nroManzana));
+		construirGrupoDeManzanasAsignables(nroManzana + 1);
+		
+		grupoActual.remove(radioCensal.getManzana(nroManzana));
+		construirGrupoDeManzanasAsignables(nroManzana + 1);
+		
 		/* Si empezamos desde las manzanas de mayor grado, seguro se van a poder armar grupos de 3
 		 * Recorrer de mayor a menor grado
 		 * Caso base, grupo.size <= 3 
@@ -45,6 +85,25 @@ public class AsignadorDeManzanasFB {
 		 */
 	}
 	
+	private boolean esCamino(ArrayList<Manzana> grupo) {
+		if(grupo.size() == 0) {
+			return false;
+		}
+		
+		if(grupo.size() == 1) {
+			return true;
+		}
+		if(grupo.size() == 2) {
+			return sonVecinas(grupo.get(0), grupo.get(1));
+		}
+		
+		return sonVecinas(grupo.get(0), grupo.get(1)) && sonVecinas(grupo.get(1), grupo.get(2));
+	}
+
+
+	private boolean sonVecinas(Manzana manzana, Manzana otraManzana) {
+		return radioCensal.manzanasVecinas(manzana.getNroManzana()).contains(otraManzana.getNroManzana());
+	}
 	private ArrayList<Manzana> obtenerArrayManzanas(RadioCensal radioCensal) {
 		Collection<Manzana> setManzanas = radioCensal.getManzanas().values();
 		return new ArrayList<Manzana>(setManzanas);
---
+++ src/tests/asignadorFB.java
@@ -0,0 +1,45 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import logica.AsignadorDeManzanasFB;
+import logica.Manzana;
+import logica.RadioCensal;
+
+public class asignadorFB {
+
+	@Test
+	public void test() {
+		RadioCensal radioCensal = new RadioCensal(9);
+
+		radioCensal.agregarManzanaContigua(new Manzana(0), new Manzana(1));
+		radioCensal.agregarManzanaContigua(new Manzana(0), new Manzana(2));
+		radioCensal.agregarManzanaContigua(new Manzana(2), new Manzana(3));
+		radioCensal.agregarManzanaContigua(new Manzana(1), new Manzana(5));
+
+		radioCensal.agregarManzanaContigua(new Manzana(3), new Manzana(4));
+		radioCensal.agregarManzanaContigua(new Manzana(4), new Manzana(5));
+		radioCensal.agregarManzanaContigua(new Manzana(3), new Manzana(6));
+		radioCensal.agregarManzanaContigua(new Manzana(5), new Manzana(8));
+		radioCensal.agregarManzanaContigua(new Manzana(4), new Manzana(7));
+
+		radioCensal.agregarManzanaContigua(new Manzana(6), new Manzana(7));
+		radioCensal.agregarManzanaContigua(new Manzana(7), new Manzana(8));
+		
+		AsignadorDeManzanasFB asignador = new AsignadorDeManzanasFB(null, radioCensal);
+		asignador.construirGrupoDeManzanasAsignables(0);
+		for(ArrayList<Manzana> manzanas : asignador.getGrupos()) {
+			System.out.print("[");
+			for(Manzana manzana : manzanas) {
+				System.out.print(manzana.getNroManzana()+", ");
+			}
+			System.out.print("], ");
+		}
+		System.out.print("\n");
+	}
+
+}
